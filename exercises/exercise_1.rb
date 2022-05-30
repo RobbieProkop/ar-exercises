eeb9c35c8440f000e7251d34fa92ccd511e9ca27
Robbie Prokop <robbieprokop@gmail.com>
2022-05-30 23:13:41 +0000

    exercise 1 complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,12 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+
+Store.create(name: "Vancouver", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+Store.create(name: "Calgary", annual_revenue: 1260000, mens_apparel: true, womens_apparel: true)
+
+Store.create(name: "Moscow", annual_revenue: 190000, mens_apparel: true, womens_apparel: true)
+
+puts Store.count
