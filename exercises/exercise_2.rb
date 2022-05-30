e224a3b3521694d4d61ac07950ad5cbdda4b616a
Robbie Prokop <robbieprokop@gmail.com>
2022-05-30 23:28:02 +0000

    exercise 2 complete

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# @store1 = SELECT id FROM stores WHERE id = 1
+@store1 = Store.find_by(id: 1)
+# @store2 = SELECT id FROM stores WHERE id = 2
+@store2 = Store.find_by(id: 2)
+@store1.name = 'Tokyo'
+
+puts @store1.name
+
